455eb2a717a8937e7a88b2ab9394e3d11fe5934a
felipeog <felipeog476@gmail.com>
2021-06-15 00:07:25 -0300

    Add Kata

---
+++ katas/snail/index.js
@@ -0,0 +1,44 @@
+const snail = (array) => {
+  const n = array[0].length;
+
+  if (n <= 0) {
+    return [];
+  }
+
+  if (n === 1) {
+    return array[0];
+  }
+
+  if (n === 2) {
+    const topRow = array.shift();
+    const bottomRow = array.pop();
+
+    return [...topRow, ...bottomRow.reverse()];
+  }
+
+  if (n >= 3) {
+    const topRow = array.shift();
+    const bottomRow = array.pop();
+
+    const { leftColumn, rightColumn } = array.reduce(
+      (acc, row) => {
+        return {
+          leftColumn: [...acc.leftColumn, row.shift()],
+          rightColumn: [...acc.rightColumn, row.pop()],
+        };
+      },
+      { leftColumn: [], rightColumn: [] }
+    );
+
+    const result = [
+      ...topRow,
+      ...rightColumn,
+      ...bottomRow.reverse(),
+      ...leftColumn.reverse(),
+    ];
+
+    return [...result, ...snail(array)];
+  }
+};
+
+module.exports = snail;
