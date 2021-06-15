40c1f5cb2a308830cc5dc6f750a461d77ba2e51b
felipeog <felipeog476@gmail.com>
2021-06-14 23:56:01 -0300

    Add utils

---
+++ utils/random.js
@@ -0,0 +1,44 @@
+const { uniqueNamesGenerator, names } = require("unique-names-generator");
+
+const generateRandomArrayIndex = (length) => {
+  return Math.floor(Math.random() * length);
+};
+
+const generateRandomName = () => {
+  return uniqueNamesGenerator({
+    dictionaries: [names],
+  });
+};
+
+const generateRandomNames = (length) => {
+  const result = [];
+  for (let i = 0; i < length; i++) {
+    result.push(generateRandomName());
+  }
+
+  return result;
+};
+
+// https://stackoverflow.com/a/2450976/10942224
+const shuffleArray = (inputArray) => {
+  const array = [...inputArray];
+  let currentIndex = array.length;
+
+  while (0 !== currentIndex) {
+    const randomIndex = Math.floor(Math.random() * currentIndex--);
+
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+};
+
+module.exports = {
+  generateRandomArrayIndex,
+  generateRandomName,
+  generateRandomNames,
+  shuffleArray,
+};
