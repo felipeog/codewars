49e232a2da338505d9635127bdb7311d4684125e
felipeog <felipeog476@gmail.com>
2021-06-16 19:02:06 -0300

    Fix return

--- utils/test.js
+++ utils/test.js
@@ -2,7 +2,7 @@ const getLoopTestTitle = (index, input, output) => {
   const inputString = JSON.stringify(input);
   const outputString = JSON.stringify(output);
 
-  return `Test ${index}: expect ${inputString} to equal ${outputString}"`;
+  return `Test ${index}: expect ${inputString} to equal ${outputString}`;
 };
 
 module.exports = {
