3e2e95649c26d6e612032cc13e6f6a38be8d0810
felipeog <felipeog476@gmail.com>
2021-06-16 18:59:04 -0300

    Refactor test

--- katas/convert-a-hex-string-to-rgb/test.js
+++ katas/convert-a-hex-string-to-rgb/test.js
@@ -1,11 +1,20 @@
+const { getLoopTestTitle } = require("../../utils/test");
 const hexStringToRGB = require("./index");
 
-describe("Tests", () => {
-  it("Test", () => {
-    expect(hexStringToRGB("#FF9933")).toEqual({ r: 255, g: 153, b: 51 });
-    expect(hexStringToRGB("#beaded")).toEqual({ r: 190, g: 173, b: 237 });
-    expect(hexStringToRGB("#000000")).toEqual({ r: 0, g: 0, b: 0 });
-    expect(hexStringToRGB("#111111")).toEqual({ r: 17, g: 17, b: 17 });
-    expect(hexStringToRGB("#Fa3456")).toEqual({ r: 250, g: 52, b: 86 });
+describe("Static tests", () => {
+  const tests = [
+    { input: "#FF9933", output: { r: 255, g: 153, b: 51 } },
+    { input: "#beaded", output: { r: 190, g: 173, b: 237 } },
+    { input: "#000000", output: { r: 0, g: 0, b: 0 } },
+    { input: "#111111", output: { r: 17, g: 17, b: 17 } },
+    { input: "#Fa3456", output: { r: 250, g: 52, b: 86 } },
+  ];
+
+  tests.forEach(({ input, output }, index) => {
+    const testTitle = getLoopTestTitle(index + 1, input, output);
+
+    it(testTitle, () => {
+      expect(hexStringToRGB(input)).toEqual(output);
+    });
   });
 });
