6d8ebb6e988e878f0ca4e35a8b72ebb65dee69f0
felipeog <felipeog476@gmail.com>
2021-06-16 20:52:10 -0300

    Refactor test

--- katas/find-the-odd-int/test.js
+++ katas/find-the-odd-int/test.js
@@ -1,43 +1,50 @@
+const { getLoopTestTitle } = require("../../utils/test");
+const { generateRandomNumber } = require("../../utils/random");
 const findOdd = require("./index");
 
-describe("Example tests", () => {
-  const exampleTests = [
+describe("Static tests", () => {
+  const tests = [
     {
-      data: [20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5],
-      expected: 5,
+      input: [20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5],
+      output: 5,
     },
-    { data: [1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5], expected: -1 },
-    { data: [20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5], expected: 5 },
-    { data: [10], expected: 10 },
-    { data: [1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1], expected: 10 },
-    { data: [5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10], expected: 1 },
+    { input: [1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5], output: -1 },
+    { input: [20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5], output: 5 },
+    { input: [10], output: 10 },
+    { input: [1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1], output: 10 },
+    { input: [5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10], output: 1 },
   ];
 
-  exampleTests.forEach(({ data, expected }, index) => {
-    test(`Example test ${index + 1}`, () => {
-      expect(findOdd(data)).toEqual(expected);
+  tests.forEach(({ input, output }, index) => {
+    const testTitle = getLoopTestTitle(index + 1, input, output);
+
+    it(testTitle, () => {
+      expect(findOdd(input)).toEqual(output);
     });
   });
 });
 
 describe("Random tests", () => {
-  var i, sz, a, j, n;
-  for (i = 0; i < 40; ++i) {
-    sz = Math.round(Math.random() * 1000 + 50);
-    if (!sz % 2) {
-      ++sz;
-    }
-    a = [];
-    for (j = 0; j < sz - 1; j += 2) {
-      n = Math.round(Math.random() * 1000);
-      a.push(n);
-      a.push(n);
-    }
-    n = Math.round(Math.random() * 1000);
-    a.push(n);
+  for (let i = 0; i < 40; ++i) {
+    const input = [];
+    const randomLength = generateRandomNumber(50, 500);
 
-    test(`Random test ${i + 1}`, () => {
-      expect(findOdd(a)).toEqual(n);
+    for (let j = 0; j < randomLength; j++) {
+      const randomNumber = generateRandomNumber(0, 1000);
+
+      input.push(randomNumber);
+      input.push(randomNumber);
+    }
+
+    const numberToFind = generateRandomNumber(0, 1000);
+    const output = numberToFind;
+
+    input.push(numberToFind);
+
+    const testTitle = getLoopTestTitle(i + 1, input, output);
+
+    it(testTitle, () => {
+      expect(findOdd(input)).toEqual(output);
     });
   }
 });
