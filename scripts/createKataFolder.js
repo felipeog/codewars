3d68b83bb0b1142f296b5824c9ce08130af698a8
felipeog <felipeog476@gmail.com>
2021-06-16 00:43:48 -0300

    Cleanup

--- scripts/createKataFolder.js
+++ scripts/createKataFolder.js
@@ -11,34 +11,28 @@ const DEFAULT_KATA_FILES = [
 ];
 
 if (!process.argv[2]) {
-  return console.error("Kata name was not provided.");
+  return console.error("ERROR: Kata name was not provided");
 }
 
 const kataName = process.argv[2];
 const folderPath = path.resolve(__dirname, `../katas/${kataName}`);
 
 if (fs.existsSync(folderPath)) {
-  return console.error("Kata already exists.");
+  return console.error("ERROR: Kata already exists");
 }
 
 fs.mkdir(folderPath, { recursive: true }, (err) => {
-  console.log("Creating Kata folder...");
   if (err) {
-    console.error("Error creating Kata folder.");
-    throw err;
+    return console.error("ERROR: Error creating Kata folder", err);
   }
-  console.log("Kata folder created.");
 
   try {
-    console.log("Creating Kata files...");
     DEFAULT_KATA_FILES.forEach(({ name, content }) => {
-      console.log(`${name} file created.`);
       fs.writeFileSync(`${folderPath}/${name}`, content);
     });
-    console.log("Kata files created.");
+
+    console.log("SUCCESS: Kata files created");
   } catch (err) {
-    // TODO: remove created folder
-    console.error("Error creating Kata files.");
-    throw err;
+    return console.error("ERROR: Error creating Kata files", err);
   }
 });
