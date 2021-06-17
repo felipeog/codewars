6683a2928b68df08b4a78d5c487ed8d58ec9e836
felipeog <felipeog476@gmail.com>
2021-06-17 19:55:16 -0300

    Refactor test

--- katas/vowel-count/test.js
+++ katas/vowel-count/test.js
@@ -1,27 +1,24 @@
+const { getLoopTestTitle } = require("../../utils/test");
 const getCount = require("./index");
 
-describe("Vowels Count Tests", () => {
-  test("Should return 5 for 'abracadabra'", () => {
-    expect(getCount("abracadabra")).toEqual(5);
-  });
+describe("Static tests", () => {
+  const tests = [
+    { input: "abracadabra", output: 5 },
+    { input: "pear tree", output: 4 },
+    { input: "o a kak ushakov lil vo kashu kakao", output: 13 },
+    { input: "my pyx", output: 0 },
+    {
+      input:
+        "tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg  k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes gjtzo bazvh tmp lkdas z ieykrma lo u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw bckzazqo uomh lbw hiwy x qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj nk mbehp dr fdjyep rhvxvwjjhzpv pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty",
+      output: 168,
+    },
+  ];
 
-  test("Should return 4 for 'pear tree'", () => {
-    expect(getCount("pear tree")).toEqual(4);
-  });
+  tests.forEach(({ input, output }, index) => {
+    const testTitle = getLoopTestTitle(index + 1, input, output);
 
-  test("Should return 13 for 'o a kak ushakov lil vo kashu kakao'", () => {
-    expect(getCount("o a kak ushakov lil vo kashu kakao")).toEqual(13);
-  });
-
-  test("Should return 0 for 'my pyx'", () => {
-    expect(getCount("my pyx")).toEqual(0);
-  });
-
-  test("Should return 168 for a long input", () => {
-    expect(
-      getCount(
-        "tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty"
-      )
-    ).toEqual(168);
+    it(testTitle, () => {
+      expect(getCount(input)).toEqual(output);
+    });
   });
 });
