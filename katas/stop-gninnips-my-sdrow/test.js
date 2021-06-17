25a29d785a031ab183d663a68bc64ff20c5ccf31
felipeog <felipeog476@gmail.com>
2021-06-17 18:45:26 -0300

    Refactor test

--- katas/stop-gninnips-my-sdrow/test.js
+++ katas/stop-gninnips-my-sdrow/test.js
@@ -1,40 +1,65 @@
+const { getLoopTestTitle } = require("../../utils/test");
+const {
+  generateRandomString,
+  generateRandomNumber,
+} = require("../../utils/random");
 const spinWords = require("./index");
 
-describe("Spinning words", () => {
-  test("Fixed tests", () => {
-    expect(spinWords("Welcome")).toEqual("emocleW");
-    expect(spinWords("Hey fellow warriors")).toEqual("Hey wollef sroirraw");
-    expect(spinWords("This is a test")).toEqual("This is a test");
-    expect(spinWords("This is another test")).toEqual("This is rehtona test");
-    expect(spinWords("This sentence is a sentence")).toEqual(
-      "This ecnetnes is a ecnetnes"
-    );
-    expect(spinWords("You are almost to the last test")).toEqual(
-      "You are tsomla to the last test"
-    );
-    expect(spinWords("Just kidding there is still one more")).toEqual(
-      "Just gniddik ereht is llits one more"
-    );
-    expect(spinWords("Seriously this is the last one")).toEqual(
-      "ylsuoireS this is the last one"
-    );
-  });
+describe("Static tests", () => {
+  const tests = [
+    { input: "Welcome", output: "emocleW" },
+    { input: "Hey fellow warriors", output: "Hey wollef sroirraw" },
+    { input: "This is a test", output: "This is a test" },
+    { input: "This is another test", output: "This is rehtona test" },
+    {
+      input: "This sentence is a sentence",
+      output: "This ecnetnes is a ecnetnes",
+    },
+    {
+      input: "You are almost to the last test",
+      output: "You are tsomla to the last test",
+    },
+    {
+      input: "Just kidding there is still one more",
+      output: "Just gniddik ereht is llits one more",
+    },
+    {
+      input: "Seriously this is the last one",
+      output: "ylsuoireS this is the last one",
+    },
+  ];
 
-  test("Random tests", () => {
-    const refSpinWords = (s) =>
-      s
-        .split(" ")
-        .map((s) => (s.length >= 5 ? s.split("").reverse().join("") : s))
-        .join(" ");
-    const rnd = (n) => Math.floor(Math.random() * n);
-    const abc = "abcdefghijklmnopqrstuvwxyz";
-    const rndWord = () =>
-      Array.from({ length: rnd(12) + 1 }, () => abc[rnd(abc.length)]).join("");
-    const rndSentence = (i) =>
-      Array.from({ length: rnd(i) }, rndWord).join(" ");
-    for (let i = 1; i <= 50; i++) {
-      const s = rndSentence(i);
-      expect(spinWords(s)).toEqual(refSpinWords(s));
-    }
+  tests.forEach(({ input, output }, index) => {
+    const testTitle = getLoopTestTitle(index + 1, input, output);
+
+    it(testTitle, () => {
+      expect(spinWords(input)).toEqual(output);
+    });
   });
+});
+
+describe("Random tests", () => {
+  const spinWordsCheck = (string) => {
+    return string
+      .split(" ")
+      .map((s) => (s.length >= 5 ? s.split("").reverse().join("") : s))
+      .join(" ");
+  };
+
+  for (let i = 0; i < 50; i++) {
+    const arrayLength = generateRandomNumber(0, 10);
+    const strings = Array.from({ length: arrayLength }, () => {
+      const stringLength = generateRandomNumber(0, 10);
+
+      return generateRandomString(stringLength);
+    });
+    const sentence = strings.join(" ");
+    const input = sentence;
+    const output = spinWordsCheck(input);
+    const testTitle = getLoopTestTitle(i + 1, input, output);
+
+    it(testTitle, () => {
+      expect(spinWords(input)).toEqual(output);
+    });
+  }
 });
