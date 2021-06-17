a4f0bbaef45c4db69de6fca7d563eeb6af181740
felipeog <felipeog476@gmail.com>
2021-06-16 22:27:00 -0300

    Refactor test

--- katas/first-non-repeating-character/test.js
+++ katas/first-non-repeating-character/test.js
@@ -1,46 +1,49 @@
+const { getLoopTestTitle } = require("../../utils/test");
 const { generateRandomName } = require("../../utils/random");
 const firstNonRepeatingLetter = require("./index");
 
-describe("Simple Tests", () => {
-  test("Should handle simple tests", () => {
-    expect(firstNonRepeatingLetter("a")).toEqual("a");
-    expect(firstNonRepeatingLetter("stress")).toEqual("t");
-    expect(firstNonRepeatingLetter("moonmen")).toEqual("e");
-  });
-  test("Should handle empty strings", () => {
-    expect(firstNonRepeatingLetter("")).toEqual("");
-  });
-});
+describe("Static Tests", () => {
+  const tests = [
+    { input: "a", output: "a" },
+    { input: "stress", output: "t" },
+    { input: "moonmen", output: "e" },
+    { input: "", output: "" },
+    { input: "abba", output: "" },
+    { input: "aa", output: "" },
+    { input: "∞§ﬁ›ﬂ∞§", output: "ﬁ" },
+    { input: "hello world, eh?", output: "w" },
+    { input: "sTreSS", output: "T" },
+    { input: "Go hang a salami, I'm a lasagna hog!", output: "," },
+  ];
 
-describe("Harder Tests", () => {
-  test("Should handle all repeating strings", () => {
-    expect(firstNonRepeatingLetter("abba")).toEqual("");
-    expect(firstNonRepeatingLetter("aa")).toEqual("");
-  });
-  test("Should handle odd characters", () => {
-    expect(firstNonRepeatingLetter("∞§ﬁ›ﬂ∞§")).toEqual("ﬁ");
-    expect(firstNonRepeatingLetter("hello world, eh?")).toEqual("w");
-  });
-  test("Should handle letter cases", () => {
-    expect(firstNonRepeatingLetter("sTreSS")).toEqual("T");
-    expect(
-      firstNonRepeatingLetter("Go hang a salami, I'm a lasagna hog!")
-    ).toEqual(",");
+  tests.forEach(({ input, output }, index) => {
+    const testTitle = getLoopTestTitle(index + 1, input, output);
+
+    it(testTitle, () => {
+      expect(firstNonRepeatingLetter(input)).toEqual(output);
+    });
   });
 });
 
 describe("Random Tests", () => {
-  var unique = generateRandomName()[0],
-    rep = new RegExp(unique, "gi"),
-    s = "",
-    temp;
-  while (s.length < 100) {
-    temp = generateRandomName().replace(rep, "");
-    s += temp + temp;
-  }
-  s += unique;
+  for (let i = 0; i < 10; i++) {
+    const uniqueLetter = generateRandomName()[0];
+    const rep = new RegExp(uniqueLetter, "gi");
+    let randomString = "";
 
-  test("Should handle random input", () => {
-    expect(firstNonRepeatingLetter(s)).toEqual(unique);
-  });
+    while (randomString.length < 100) {
+      const randomName = generateRandomName().replace(rep, "");
+      randomString += `${
+        !randomString.length ? "" : " "
+      }${randomName} ${randomName}`;
+    }
+
+    const input = randomString + uniqueLetter;
+    const output = uniqueLetter;
+    const testTitle = getLoopTestTitle(i + 1, input, output);
+
+    it(testTitle, () => {
+      expect(firstNonRepeatingLetter(input)).toEqual(output);
+    });
+  }
 });
