9a62ce47107bf6b4c65584deed584c7f4a313d97
felipeog <felipeog476@gmail.com>
2021-06-17 19:03:16 -0300

    Refactor test

--- katas/sum-of-digits-digital-root/test.js
+++ katas/sum-of-digits-digital-root/test.js
@@ -1,26 +1,40 @@
+const { getLoopTestTitle } = require("../../utils/test");
+const { generateRandomNumber } = require("../../utils/random");
 const digital_root = require("./index");
 
-describe("Basic tests", () => {
-  test("Should pass basic tests", () => {
-    expect(digital_root(16)).toEqual(7);
-    expect(digital_root(195)).toEqual(6);
-    expect(digital_root(992)).toEqual(2);
-    expect(digital_root(999999999999)).toEqual(9);
-    expect(digital_root(167346)).toEqual(9);
-    expect(digital_root(10)).toEqual(1);
-    expect(digital_root(0)).toEqual(0);
+describe("Static tests", () => {
+  const tests = [
+    { input: 0, output: 0 },
+    { input: 10, output: 1 },
+    { input: 16, output: 7 },
+    { input: 195, output: 6 },
+    { input: 992, output: 2 },
+    { input: 167_346, output: 9 },
+    { input: 999_999_999_999, output: 9 },
+  ];
+
+  tests.forEach(({ input, output }, index) => {
+    const testTitle = getLoopTestTitle(index + 1, input, output);
+
+    it(testTitle, () => {
+      expect(digital_root(input)).toEqual(output);
+    });
   });
 });
 
 describe("Random tests", () => {
-  test("Should pass random tests", () => {
-    const digital_root_sol = (n) => {
-      return n > 0 ? 1 + ((parseInt(n) - 1) % 9) : 0;
-    };
+  const digital_rootCheck = (n) => {
+    return n > 0 ? 1 + ((parseInt(n) - 1) % 9) : 0;
+  };
 
-    for (let i = 0; i < 100; i++) {
-      let n = Math.floor(Math.random() * 1000000);
-      expect(digital_root(n)).toEqual(digital_root_sol(n));
-    }
-  });
+  for (let i = 0; i < 100; i++) {
+    const randomNumber = generateRandomNumber(0, 1_000_000);
+    const input = randomNumber;
+    const output = digital_rootCheck(randomNumber);
+    const testTitle = getLoopTestTitle(i + 1, input, output);
+
+    it(testTitle, () => {
+      expect(digital_root(input)).toEqual(output);
+    });
+  }
 });
