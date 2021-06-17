40aac2269e5ef8bc5dcea6d0edc883dc79ef9559
felipeog <felipeog476@gmail.com>
2021-06-16 23:10:39 -0300

    Refactor test

--- katas/is-integer-array/test.js
+++ katas/is-integer-array/test.js
@@ -1,22 +1,31 @@
+const { getLoopTestTitle } = require("../../utils/test");
 const isIntArray = require("./index");
 
 describe("Basic tests", () => {
-  test("Tests", () => {
-    expect(isIntArray([])).toEqual(true);
-    expect(isIntArray([1, 2, 3, 4])).toEqual(true);
-    expect(isIntArray([-11, -12, -13, -14])).toEqual(true);
-    expect(isIntArray([1.0, 2.0, 3.0])).toEqual(true);
-    expect(isIntArray([1, 2, NaN])).toEqual(false);
-    expect(isIntArray(null)).toEqual(false);
-    expect(isIntArray(undefined)).toEqual(false);
-    expect(isIntArray(NaN)).toEqual(false);
-    expect(isIntArray("")).toEqual(false);
-    expect(isIntArray([null])).toEqual(false);
-    expect(isIntArray([undefined])).toEqual(false);
-    expect(isIntArray([NaN])).toEqual(false);
-    expect(isIntArray([1.0, 2.0, 3.0001])).toEqual(false);
-    expect(isIntArray(["-1"])).toEqual(false);
-    expect(isIntArray([1.23e-7, 2])).toEqual(false);
-    expect(isIntArray([1.2, 1.8, 3])).toEqual(false);
+  const tests = [
+    { input: [], output: true },
+    { input: [1, 2, 3, 4], output: true },
+    { input: [-11, -12, -13, -14], output: true },
+    { input: [1.0, 2.0, 3.0], output: true },
+    { input: [1, 2, NaN], output: false },
+    { input: null, output: false },
+    { input: undefined, output: false },
+    { input: NaN, output: false },
+    { input: "", output: false },
+    { input: [null], output: false },
+    { input: [undefined], output: false },
+    { input: [NaN], output: false },
+    { input: [1.0, 2.0, 3.0001], output: false },
+    { input: ["-1"], output: false },
+    { input: [1.23e-7, 2], output: false },
+    { input: [1.2, 1.8, 3], output: false },
+  ];
+
+  tests.forEach(({ input, output }, index) => {
+    const testTitle = getLoopTestTitle(index + 1, input, output);
+
+    it(testTitle, () => {
+      expect(isIntArray(input)).toEqual(output);
+    });
   });
 });
