7a2f200374174c4b6b610b3357838cafd62f7061
felipeog <felipeog476@gmail.com>
2021-06-17 15:32:34 -0300

    Refactor test

--- katas/next-bigger-number-with-the-same-digits/test.js
+++ katas/next-bigger-number-with-the-same-digits/test.js
@@ -1,53 +1,83 @@
+const { getLoopTestTitle } = require("../../utils/test");
+const { generateRandomNumber } = require("../../utils/random");
 const nextBigger = require("./index");
 
-describe("Basic tests", () => {
-  test("Small numbers", () => {
-    expect(nextBigger(12)).toEqual(21);
-    expect(nextBigger(513)).toEqual(531);
-    expect(nextBigger(2017)).toEqual(2071);
-    expect(nextBigger(414)).toEqual(441);
-    expect(nextBigger(144)).toEqual(414);
-  });
+describe("Static tests", () => {
+  const tests = [
+    { input: 12, output: 21 },
+    { input: 144, output: 414 },
+    { input: 414, output: 441 },
+    { input: 513, output: 531 },
+    { input: 2_017, output: 2_071 },
+    { input: 123_456_789, output: 123_456_798 },
+    { input: 1_234_567_890, output: 1_234_567_908 },
+    { input: 9_876_543_210, output: -1 },
+    { input: 9_999_999_999, output: -1 },
+    { input: 59_884_848_459_853, output: 59_884_848_483_559 },
+  ];
 
-  test("Bigger numbers", () => {
-    expect(nextBigger(123456789)).toEqual(123456798);
-    expect(nextBigger(1234567890)).toEqual(1234567908);
-    expect(nextBigger(9876543210)).toEqual(-1);
-    expect(nextBigger(9999999999)).toEqual(-1);
-    expect(nextBigger(59884848459853)).toEqual(59884848483559);
+  tests.forEach(({ input, output }, index) => {
+    const testTitle = getLoopTestTitle(index + 1, input, output);
+
+    it(testTitle, () => {
+      expect(nextBigger(input)).toEqual(output);
+    });
   });
 });
 
 describe("Random tests", () => {
-  function randint(a, b) {
-    return Math.floor(Math.random() * (b - a + 1) + a);
-  }
-  function nextSol(n) {
-    n = n.toString().split("");
-    let temp = [],
-      i = n.length - 1;
-    while (i && n[i - 1] >= n[i]) {
-      temp.push(n[i--]);
+  const nextSol = (number) => {
+    const numberDigits = [...number.toString()];
+    let temp = [];
+    let i = numberDigits.length - 1;
+
+    while (i && numberDigits[i - 1] >= numberDigits[i]) {
+      temp.push(numberDigits[i--]);
     }
-    temp.push(n[i--]);
-    if (i == -1) return i;
-    let top = n[i],
-      j = +top + 1;
-    while (temp.indexOf("" + j) == -1) j++;
+
+    temp.push(numberDigits[i--]);
+
+    if (i === -1) {
+      return i;
+    }
+
+    const top = numberDigits[i];
+    let j = +top + 1;
+
+    while (temp.indexOf("" + j) === -1) {
+      j++;
+    }
+
     j = "" + j;
+
     temp.splice(temp.indexOf(j), 1);
     temp.push(top);
     temp.sort();
     temp.unshift(j);
-    return +n.slice(0, i).concat(temp).join("");
-  }
-  for (let _ = 0; _ < 140; _++) {
-    let n = randint(1, Math.pow(10, randint(1, 15)));
-    if (randint(1, 100) < 10)
-      n = +n.toString().split("").sort().reverse().join("").slice(-15);
 
-    test("Testing for " + n, () => {
-      expect(nextBigger(n)).toEqual(nextSol(n));
+    return +numberDigits.slice(0, i).concat(temp).join("");
+  };
+
+  for (let i = 0; i < 140; i++) {
+    const randomMax = 10 ** generateRandomNumber(1, 15);
+    let randomNumber = generateRandomNumber(1, randomMax);
+
+    if (Math.random() < 0.1) {
+      randomNumber = +randomNumber
+        .toString()
+        .split("")
+        .sort()
+        .reverse()
+        .join("")
+        .slice(-15);
+    }
+
+    const input = randomNumber;
+    const output = nextSol(randomNumber);
+    const testTitle = getLoopTestTitle(i + 1, input, output);
+
+    it(testTitle, () => {
+      expect(nextBigger(input)).toEqual(output);
     });
   }
 });
