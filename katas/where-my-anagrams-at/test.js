27891445e517f573090c698f5a4e3762a7f36205
felipeog <felipeog476@gmail.com>
2021-06-17 20:38:17 -0300

    Refactor test

--- katas/where-my-anagrams-at/test.js
+++ katas/where-my-anagrams-at/test.js
@@ -1,14 +1,24 @@
+const { getLoopTestTitle } = require("../../utils/test");
 const anagrams = require("./index");
 
-describe("Basic tests", () => {
-  test("Tests", () => {
-    expect(anagrams("abba", ["aabb", "abcd", "bbaa", "dada"])).toEqual([
-      "aabb",
-      "bbaa",
-    ]);
-    expect(
-      anagrams("racer", ["crazer", "carer", "racar", "caers", "racer"])
-    ).toEqual(["carer", "racer"]);
-    expect(anagrams("laser", ["lazing", "lazy", "lacer"])).toEqual([]);
+describe("Static tests", () => {
+  const tests = [
+    {
+      input: ["abba", ["aabb", "abcd", "bbaa", "dada"]],
+      output: ["aabb", "bbaa"],
+    },
+    {
+      input: ["racer", ["crazer", "carer", "racar", "caers", "racer"]],
+      output: ["carer", "racer"],
+    },
+    { input: ["laser", ["lazing", "lazy", "lacer"]], output: [] },
+  ];
+
+  tests.forEach(({ input, output }, index) => {
+    const testTitle = getLoopTestTitle(index + 1, input, output);
+
+    it(testTitle, () => {
+      expect(anagrams(...input)).toEqual(output);
+    });
   });
 });
