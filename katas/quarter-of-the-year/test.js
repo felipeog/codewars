149de825809c962669d203e893d7ab2183b132bf
felipeog <felipeog476@gmail.com>
2021-06-17 16:40:11 -0300

    Refactor test

--- katas/quarter-of-the-year/test.js
+++ katas/quarter-of-the-year/test.js
@@ -1,24 +1,36 @@
+const { getLoopTestTitle } = require("../../utils/test");
+const { generateRandomNumber } = require("../../utils/random");
 const quarterOf = require("./index");
 
-describe("Basic Tests", () => {
-  test("Month 3 = quarter 1", () => {
-    expect(quarterOf(3)).toEqual(1);
-  });
-  test("Month 8 = quarter 3", () => {
-    expect(quarterOf(8)).toEqual(3);
-  });
-  test("Month 11 = quarter 4", () => {
-    expect(quarterOf(11)).toEqual(4);
+describe("Static tests", () => {
+  const tests = [
+    { input: 3, output: 1 },
+    { input: 8, output: 3 },
+    { input: 11, output: 4 },
+  ];
+
+  tests.forEach(({ input, output }, index) => {
+    const testTitle = getLoopTestTitle(index + 1, input, output);
+
+    it(testTitle, () => {
+      expect(quarterOf(input)).toEqual(output);
+    });
   });
 });
 
-describe("Random Tests", () => {
-  const quarterOfCheck = (month) => Math.ceil(month / 3);
+describe("Random tests", () => {
+  const quarterOfCheck = (month) => {
+    return Math.ceil(month / 3);
+  };
 
-  test("Testing for 100 random tests", () => {
-    for (let i = 0; i < 100; i++) {
-      const rnd = Math.floor(Math.random() * 12 + 1);
-      expect(quarterOf(rnd)).toEqual(quarterOfCheck(rnd));
-    }
-  });
+  for (let i = 0; i < 100; i++) {
+    const randomMonth = generateRandomNumber(1, 12);
+    const input = randomMonth;
+    const output = quarterOfCheck(randomMonth);
+    const testTitle = getLoopTestTitle(i + 1, input, output);
+
+    it(testTitle, () => {
+      expect(quarterOf(input)).toEqual(output);
+    });
+  }
 });
