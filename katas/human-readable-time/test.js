0e0e15c1419b23ad019aab4c4884e2db429188c0
felipeog <felipeog476@gmail.com>
2021-06-16 23:01:33 -0300

    Refactor test

--- katas/human-readable-time/test.js
+++ katas/human-readable-time/test.js
@@ -1,16 +1,25 @@
+const { getLoopTestTitle } = require("../../utils/test");
 const humanReadable = require("./index");
 
-describe("Tests", () => {
-  test("Should format correctly", () => {
-    expect(humanReadable(0)).toEqual("00:00:00");
-    expect(humanReadable(59)).toEqual("00:00:59");
-    expect(humanReadable(60)).toEqual("00:01:00");
-    expect(humanReadable(90)).toEqual("00:01:30");
-    expect(humanReadable(3599)).toEqual("00:59:59");
-    expect(humanReadable(3600)).toEqual("01:00:00");
-    expect(humanReadable(45296)).toEqual("12:34:56");
-    expect(humanReadable(86399)).toEqual("23:59:59");
-    expect(humanReadable(86400)).toEqual("24:00:00");
-    expect(humanReadable(359999)).toEqual("99:59:59");
+describe("Static tests", () => {
+  const tests = [
+    { input: 0, output: "00:00:00" },
+    { input: 59, output: "00:00:59" },
+    { input: 60, output: "00:01:00" },
+    { input: 90, output: "00:01:30" },
+    { input: 3599, output: "00:59:59" },
+    { input: 3600, output: "01:00:00" },
+    { input: 45296, output: "12:34:56" },
+    { input: 86399, output: "23:59:59" },
+    { input: 86400, output: "24:00:00" },
+    { input: 359999, output: "99:59:59" },
+  ];
+
+  tests.forEach(({ input, output }, index) => {
+    const tesTitle = getLoopTestTitle(index + 1, input, output);
+
+    test(tesTitle, () => {
+      expect(humanReadable(input)).toEqual(output);
+    });
   });
 });
